fee40e144248980ae7d0717fa92b21494de096d5
bagoom <bwj2517@naver.com>
2022-04-27 16:04:52 +0900

    theme2 - 모바일 스타일 적용

--- theme2/head.php
+++ theme2/head.php
@@ -37,7 +37,7 @@ if (!file_exists($reunion_config_file)) {
         include G5_BBS_PATH.'/newwin.inc.php'; // 팝업레이어
     }
     ?>
-    <div id="tnb">
+    <div id="tnb" class="pc-show">
         <div class="inner">
 			<ul id="hd_qnb">
                 <?php if ($is_member) {  ?>
@@ -58,7 +58,85 @@ if (!file_exists($reunion_config_file)) {
         <div id="logo">
             <a href="<?php echo G5_URL ?>"><img src="<?php echo G5_IMG_URL ?>/logo.gif" alt="<?php echo $config['cf_title']; ?>"></a>
         </div>
-        <nav id="gnb">
+
+        <div class="nav-open-btn m-show">
+            <i class="xi-bars"></i>
+        </div>
+        <nav id="gnb" class="pc-show">
+            <h2>메인메뉴</h2>
+            <div class="gnb_wrap">
+                <ul id="gnb_1dul">
+                    <?php
+                    $menu_datas = get_menu_db(0, true);
+                    $gnb_zindex = 999; // gnb_1dli z-index 값 설정용
+                    $i = 0;
+                    foreach( $menu_datas as $row ){
+                        if( empty($row) ) continue;
+                        $add_class = (isset($row['sub']) && $row['sub']) ? 'gnb_al_li_plus' : '';
+                    ?>
+                    <li class="gnb_1dli <?php echo $add_class; ?>" style="z-index:<?php echo $gnb_zindex--; ?>">
+                        <a href="<?=G5_URL?><?php echo $row['me_link']; ?>" target="_<?php echo $row['me_target']; ?>" class="gnb_1da"><?php echo $row['me_name'] ?></a>
+                    </li>
+                    <?php
+                    $i++;
+                    }   //end foreach $row
+    
+                    if ($i == 0) {  ?>
+                        <li class="gnb_empty">메뉴 준비 중입니다.<?php if ($is_admin) { ?> <a href="<?php echo G5_ADMIN_URL; ?>/menu_list.php">관리자모드 &gt; 환경설정 &gt; 메뉴설정</a>에서 설정하실 수 있습니다.<?php } ?></li>
+                    <?php } ?>
+                </ul>
+                <div id="gnb_all">
+                    <div class="inner-wrapper">
+                    <ul class="gnb_al_ul">
+                        <?php
+                        
+                        $i = 0;
+                        foreach( $menu_datas as $row ){
+                        ?>
+                        <li class="gnb_al_li">
+                            <!-- <a href="<?php echo $row['me_link']; ?>" target="_<?php echo $row['me_target']; ?>" class="gnb_al_a"><?php echo $row['me_name'] ?></a> -->
+                            <?php
+                            $k = 0;
+                            foreach( (array) $row['sub'] as $row2 ){
+                                if($k == 0)
+                                    echo '<ul>'.PHP_EOL;
+                            ?>
+                                <li><a href="<?=G5_URL?><?php echo $row2['me_link']; ?>" target="_<?php echo $row2['me_target']; ?>"><?php echo $row2['me_name'] ?></a></li>
+                            <?php
+                            $k++;
+                            }   //end foreach $row2
+    
+                            if($k > 0)
+                                echo '</ul>'.PHP_EOL;
+                            ?>
+                        </li>
+                        <?php
+                        $i++;
+                        }   //end foreach $row
+    
+                        if ($i == 0) {  ?>
+                            <li class="gnb_empty">메뉴 준비 중입니다.<?php if ($is_admin) { ?> <br><a href="<?php echo G5_ADMIN_URL; ?>/menu_list.php">관리자모드 &gt; 환경설정 &gt; 메뉴설정</a>에서 설정하실 수 있습니다.<?php } ?></li>
+                        <?php } ?>
+                    </ul>
+                    </div>
+                </div>
+            </div>
+        </nav>
+        <nav id="gnb" class="m-show">
+            <div id="tnb" >
+                <div class="inner">
+                    <ul id="hd_qnb">
+                        <?php if ($is_member) {  ?>
+                            <li><a href="<?php echo G5_BBS_URL ?>/member_confirm.php?url=<?php echo G5_BBS_URL ?>/register_form.php">정보수정</a></li>
+                            <li><a href="<?php echo G5_BBS_URL ?>/logout.php">로그아웃</a></li>
+                            <?php } else {  ?>
+                            <li><a href="<?php echo G5_BBS_URL ?>/register.php">회원가입</a></li>
+                            <li><a href="<?php echo G5_BBS_URL ?>/login.php">로그인</a></li>
+                        <?php }  ?>
+                    </ul>
+                </div>
+            </div>
+            <div class="nav-close-btn"><i class="xi-close"></i></div>
             <h2>메인메뉴</h2>
             <div class="gnb_wrap">
                 <ul id="gnb_1dul">
@@ -153,6 +231,14 @@ if (!file_exists($reunion_config_file)) {
                 easing: "easeOutBack"
             });
         });
+
+        $(".nav-open-btn").click(function(){
+            $("#gnb.m-show").addClass("on");
+        });
+        $(".nav-close-btn").click(function(){
+            $("#gnb.m-show").removeClass("on");
+        });
+
     });
 
     </script>
--- theme2/head.sub.php
+++ theme2/head.sub.php
@@ -2,21 +2,14 @@
 // 이 파일은 새로운 파일 생성시 반드시 포함되어야 함
 if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 
-// 테마 head.sub.php 파일
-if(!defined('G5_IS_ADMIN') && defined('G5_THEME_PATH') && is_file(G5_THEME_PATH.'/head.sub.php')) {
-    require_once(G5_THEME_PATH.'/head.sub.php');
-    return;
-}
-
 $g5_debug['php']['begin_time'] = $begin_time = get_microtime();
-
 if (!isset($g5['title'])) {
-    $g5['title'] = $config['cf_title'];
-    $g5_head_title = $g5['title'];
+    $g5['title'] = $reunion['reunion_title'];
+    $g5_head_title = $reunion['reunion_title'];
 }
 else {
-    // 상태바에 표시될 제목
-    $g5_head_title = implode(' | ', array_filter(array($g5['title'], $config['cf_title'])));
+    $g5_head_title = $g5['title']; // 상태바에 표시될 제목
+    $g5_head_title .= " | ".$reunion['reunion_title'];
 }
 
 $g5['title'] = strip_tags($g5['title']);
@@ -43,10 +36,10 @@ header("Pragma: no-cache"); // HTTP/1.0
 <meta charset="utf-8">
 <?php
 if (G5_IS_MOBILE) {
-    echo '<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=0,maximum-scale=10">'.PHP_EOL;
     echo '<meta name="HandheldFriendly" content="true">'.PHP_EOL;
     echo '<meta name="format-detection" content="telephone=no">'.PHP_EOL;
 } else {
+    echo '<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=0,maximum-scale=10">'.PHP_EOL;
     echo '<meta http-equiv="imagetoolbar" content="no">'.PHP_EOL;
     echo '<meta http-equiv="X-UA-Compatible" content="IE=Edge">'.PHP_EOL;
 }
@@ -111,6 +104,7 @@ if(!defined('G5_IS_ADMIN'))
 ?>
 <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css">
 <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&amp;display=swap" rel="stylesheet">
+<link rel="stylesheet" href="<?=G5_CSS_URL?>/responsive.css">
 <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/xeicon@2.3.3/xeicon.min.css">
 </head>
 <body<?php echo isset($g5['body_script']) ? $g5['body_script'] : ''; ?>>
